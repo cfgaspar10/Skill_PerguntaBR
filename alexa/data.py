43950afd5e54dd75e16c3828c8b05d4a2a3ad649
cfgaspar10 <carlesandrogaspar@gmail.com>
2020-03-14 15:30:53 +0000

    ïncluir AC

--- alexa/data.py
+++ alexa/data.py
@@ -5,4 +5,9 @@ STATES_LIST = [
         'abreviatura': 'TO', 'capital': 'Palmas',
         'estado': 'Tocantins', 'ordem_estados': '26',
         'ano_fundacao': '1988'
+    },
+    {
+        'abreviatura': 'AC', 'capital': 'Rio Branco',
+        'estado': 'Acre', 'ordem_estados': '1',
+        'ano_fundacao': '1962'
     }]
