9ef224b6884e34065b3ae98cfb9b3c6c92913413
cfgaspar10 <carlesandrogaspar@gmail.com>
2020-03-14 14:14:47 +0000

    inicio_projeto

---
+++ alexa/data.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+STATES_LIST = [
+    {
+        'abreviatura': 'TO', 'capital': 'Palmas',
+        'estado': 'Tocantins', 'statehood_order': '26',
+        'statehood_year': '1988'
+    },
---
+++ alexa/util.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import random
+import six
+from ask_sdk_core.handler_input import HandlerInput
+from ask_sdk_core.utils import is_request_type
+
+from . import data
+
+
+def get_random_state(states_list):
+    """Return a random value from the list of states."""
+    return random.choice(states_list)
